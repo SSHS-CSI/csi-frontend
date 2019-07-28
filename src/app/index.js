4727fa5898b903286b811a8fb919e6d3fbe79489
Sungbin Jo <pcr910303@icloud.com>
2019-07-29 00:27:30 +0900

    Add new Class component

--- src/app/index.js
+++ src/app/index.js
@@ -3,13 +3,21 @@ const { useState } = React;
 
 const AppBar = require("./appbar.js");
 const Drawer = require("./drawer.js");
+const Class = require("./class.js");
 
 const App = () => {
     const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+    const [isClassDialogOpen, setIsClassDialogOpen] = useState(false);
     return (
         <div>
             <AppBar onMenuClick={() => setIsDrawerOpen(true)} />
             <Drawer open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)} />
+            <Class name="객체지향" open={isClassDialogOpen}
+                   onClose={() => setIsClassDialogOpen(false)} teacher="박미영"
+                   students={["조성빈", "신기준", "권현우"]} assignments={[{
+                       title: "연습문제 1",
+                       deadline: new Date("Sun Jul 30 2019"),
+                   }]}/>
         </div>
     );
 };
