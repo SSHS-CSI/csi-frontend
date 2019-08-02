0dc9fb3ac6292d3951707a2decb8cff74d09b3ce
Sungbin Jo <pcr910303@icloud.com>
2019-08-02 18:01:44 +0900

    Add Assignment component

---
+++ src/app/assignment.js
@@ -0,0 +1,31 @@
+const React = require("react");
+
+const Table = require("@material-ui/core/Table").default;
+const TableBody = require("@material-ui/core/TableBody").default;
+const TableCell = require("@material-ui/core/TableCell").default;
+const TableRow = require("@material-ui/core/TableRow").default;
+
+const ClosableDialog = require("./closable-dialog.js");
+
+module.exports = ({ assignment: { title, content, deadline }, ...props }) => {
+    return (
+        <ClosableDialog {...props}>
+            <Table>
+                <TableBody>
+                    <TableRow>
+                        <TableCell component="th" scope="row" variant="head">제목</TableCell>
+                        <TableCell>{title}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell component="th" scope="row" variant="head">내용</TableCell>
+                        <TableCell>{content}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell component="th" scope="row" variant="head">마감</TableCell>
+                        <TableCell>{deadline && deadline.toLocaleDateString()}</TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </ClosableDialog>
+    );
+};
--- src/app/index.js
+++ src/app/index.js
@@ -4,10 +4,12 @@ const { useState } = React;
 const AppBar = require("./appbar.js");
 const Drawer = require("./drawer.js");
 const Class = require("./class.js");
+const Assignment = require("./assignment.js");
 
 const App = () => {
     const [isDrawerOpen, setIsDrawerOpen] = useState(false);
     const [isClassDialogOpen, setIsClassDialogOpen] = useState(false);
+    const [isAssignmentDialogOpen, setIsAssignmentDialogOpen] = useState(true);
     return (
         <div>
             <AppBar onMenuClick={() => setIsDrawerOpen(true)} />
@@ -18,6 +20,13 @@ const App = () => {
                        title: "연습문제 1",
                        deadline: new Date("Sun Jul 30 2019"),
                    }]}/>
+            <Assignment open={isAssignmentDialogOpen} title="과제" onClose={() => setIsAssignmentDialogOpen(false)}
+                        assignment={{
+                            title: "연습문제 1",
+                            content: "연습문제 1의 3번 문제부터 16번 문제까지 풀기",
+                            deadline: new Date("Sun Jul 30 2019"),
+                            author: "조성빈"
+                        }} />
         </div>
     );
 };
