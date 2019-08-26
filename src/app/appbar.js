dfc41c84497e30c82e9e47109542a6cc0164779b
Sungbin Jo <pcr910303@icloud.com>
2019-08-26 17:59:31 +0900

    Add a login dialog

--- src/app/appbar.js
+++ src/app/appbar.js
@@ -1,4 +1,5 @@
 const React = require("react");
+const { useState } = React;
 
 const { makeStyles } = require("@material-ui/core/styles");
 
@@ -7,13 +8,25 @@ const Toolbar = require("@material-ui/core/Toolbar").default;
 const IconButton = require("@material-ui/core/IconButton").default;
 const MenuIcon = require("@material-ui/icons/Menu").default;
 const Typography = require("@material-ui/core/Typography").default;
+const Button = require("@material-ui/core/Button").default;
+const TextField = require("@material-ui/core/TextField").default;
 
-const useStyles = makeStyles(theme => ({ menu: { marginRight: theme.spacing() } }));
+const ClosableDialog = require("./closable-dialog.js");
+
+const useStyles = makeStyles(theme => ({
+    menu: { marginRight: theme.spacing() },
+    title: { flexGrow: 1 },
+    loginDialog: {
+        display: "flex",
+        flexDirection: "column"
+    }
+}));
 
 module.exports = ({
     onMenuClick, ...props
 }) => {
     const classes = useStyles();
+    const [isLoginDialogOpen, setIsLoginDialogOpen] = useState(false);
 
     return (
         <AppBar {...props}>
@@ -21,7 +34,14 @@ module.exports = ({
                 <IconButton edge="start" color="inherit" className={classes.menu} onClick={onMenuClick}>
                     <MenuIcon />
                 </IconButton>
-                <Typography variant="h6">Jirung World</Typography>
+                <Typography variant="h6" className={classes.title}>Jirung World</Typography>
+                <Button color="inherit" onClick={() => setIsLoginDialogOpen(true)}>로그인</Button>
+                <ClosableDialog title="로그인" open={isLoginDialogOpen}  onClose={() => setIsLoginDialogOpen(false)}>
+                    <div className={classes.loginDialog}>
+                        <TextField type="email" placeholder="Email" margin="normal" autoFocus />
+                        <TextField type="password" placeholder="Password" margin="normal" />
+                    </div>
+                </ClosableDialog>
             </Toolbar>
         </AppBar>
     );
